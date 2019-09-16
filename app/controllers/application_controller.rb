7886bca6411b522873a65716aed4de5cbdc9a35e
Ranika Williams <rnkprice07@gmail.com>
2019-09-16 15:16:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
       Members.new(member_data["name"], member_data["power"], member_data["bio"])
     end
     
-    @team = 
+    @team = Team.new(params["team"]["name"], )
       
     erb :team
   end
